550c3b5cf94cd31b89824f9ceb84f62f76b32c1e
chensansui-cc <69887941+chensansui-cc@users.noreply.github.com>
2026-07-12 04:03:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,16 +4,13 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# 修改默认 IP（可选，比如修改为 192.168.1.1）
+# 修改默认 IP 为 192.168.10.1
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Add a feed source
+# 添加 helloworld 软件源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 添加 Passwall 2 主程序源和依赖包源（必须有这两行，才能编译出 Passwall 2！）
+echo 'src-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
